42d5b3496620768f1363e5a64b91cac4234ee1b6
qcxydyt <104046346+qcxydyt@users.noreply.github.com>
2026-01-08 22:45:23 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const TEMPLATE_CONFIG = [
   {
-    id: '0001',
+    id: '	KUuRLJN4g_JtOlAhO0utlQH5_RVl-5EqvcQ6HH9A1Nw',
     title: '亲爱的, 早上好',
     desc: `
       **{{date.DATA}}**
